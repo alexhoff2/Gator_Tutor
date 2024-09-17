05c7813149bd5c287034fbda603cf8bfe68ec027
Jack Richards <jackrichards@me.com>
2024-09-17 11:28:03 -0700

    Added basic starting point website structure with login and about page

--- application/index.js
+++ application/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+// Serve static files (CSS, JS, images) from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the homepage
 app.get('/', (req, res) => {
-  res.send('Hello, Team 6!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Route for the About page
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+// Start the server
 app.listen(port, '0.0.0.0', () => {
   console.log(`App listening at http://localhost:${port}`);
 });
-
-
