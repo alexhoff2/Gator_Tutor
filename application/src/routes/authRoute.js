105db5267306ffb393294bbb935caeebecb14765
Jack Richards <jackrichards@me.com>
2024-09-29 18:21:09 -0700

    feat: Add authentication route
    Added a new file, authRoute.js, to handle authentication routes. This file includes routes for user registration, login, and logout. It also imports the corresponding controllers from the authController.js file to handle the logic for each route.

---
+++ application/src/routes/authRoute.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
+
+// GET register Page
+router.get("/register", authController.getregisterPage);
+
+// POST register Form
+router.post("/register", authController.postregisterForm);
+
+// GET Login Page
+router.get("/login", authController.getLoginPage);
+
+// POST Login Form
+router.post("/login", authController.postLoginForm);
+
+// GET Logout
+router.get("/logout", (req, res) => {
+  console.log("GET /logout");
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send("Error logging out");
+    }
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
